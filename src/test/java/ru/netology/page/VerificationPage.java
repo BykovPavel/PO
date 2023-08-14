a583ea9dfc254f101e2a5f84d94fd8bfa131be5e
Pavel Bykov <pavelbyk@outlook.com>
2023-08-14 14:56:24 +0300

    Initial commit

---
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -0,0 +1,21 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+public class VerificationPage {
+    private final SelenideElement codeField = Selenide.$("[data-test-id=code] input");
+    private final SelenideElement verifyButton = Selenide.$("[data-test-id=action-verify]");
+
+    public VerificationPage() {
+        codeField.shouldBe(Condition.visible);
+    }
+
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage();
+    }
+}
